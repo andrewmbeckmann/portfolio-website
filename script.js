16390361ccb000048c3ec0832d8f8093cb42f0ef
andrewmbeckmann <andrewmbeckmann@email.com>
2024-12-16 06:01:33 -0500

    game loop complete + better code detect

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 var isLight = false; 
-let lastKeyTime = lastSwingTime = 0;
+let lastKeyTime = lastSwingTime = lastHurtTime = 0;
 let keyArray = [];
 let gameStarted = false;
 const code = "8045"
 const swingCooldown = 500;
 const frameTime = 100;
+const maxLives = 3;
 let movementIncrement = 3;
 let bugMovement = 2;
 let playerSize = 16;
-let playerX = 0;
-let playerY = 0; 
+let playerX = playerY = hitsTaken = 0;
 let goingUp = goingDown = goingLeft = goingRight = false;
 
 document.getElementById("darklight").addEventListener("click", () => {
+    let audio = new Audio('audio/switchsound.mp3');
+    audio.play();
     if(!isLight) {
         isLight = true;
         document.getElementById("body").classList.add("light");
@@ -56,6 +58,7 @@ window.addEventListener("keyup", e => {
 
     keyArray.push(key);
 
+    if(keyArray.length > 4) keyArray = keyArray.slice(keyArray.length - 4);
     if(keyArray.join('') === code && !gameStarted){
         createGame();
     }
@@ -131,6 +134,7 @@ function createGame(){
 
 function endGame() {
     gameStarted = false;
+    hitsTaken = 0;
     let elem = document.getElementById("player");
     elem.remove();
     bugSpray();
@@ -160,6 +164,7 @@ function runGame(){
             if (goingLeft) movePlayer(-movementIncrement, 0);
             if (goingRight) movePlayer(movementIncrement, 0);
             updateSwordPos();
+            checkPlayerCollision();
             if (!gameStarted) clearInterval(gameTicks);
         }
 }
@@ -199,12 +204,7 @@ function updateSwordPos(){
         elem.classList.add("sworddown");
         elem.style.left = playerX + "px";
         elem.style.top = playerY + playerSize + "px";
-    } else {
-        elem.classList.remove(...elem.classList);
-        elem.classList.add("swordright");
-        elem.style.left = playerX + playerSize + "px";
-        elem.style.top = playerY + "px";
-    }
+    } 
 
 }
 
@@ -213,21 +213,60 @@ function swingSword(){
     if (currentSwingTime - lastSwingTime < swingCooldown) return;
     let elem = document.getElementById("sword")
     
+    let audio = new Audio('audio/swordswing.mp3');
+    audio.play();
+    
     elem.setAttribute("src", "images/swingframes/1.png")
     setTimeout(() => {
         elem.setAttribute("src", "images/swingframes/2.png")
     }, frameTime)
     setTimeout(() => {
         elem.setAttribute("src", "images/swingframes/3.png")
+        checkSwordCollision();
     }, frameTime*2)
     setTimeout(() => {
         elem.setAttribute("src", "images/swingframes/4.png")
+        checkSwordCollision();
     }, frameTime*3)
     setTimeout(() => {
         elem.setAttribute("src", "images/swordbase.png")
+        checkSwordCollision();
     }, frameTime*4)
 
     lastSwingTime = currentSwingTime;
+}
+
+function checkSwordCollision(){
+    let bugList = document.getElementsByClassName("bug");
+    let sword = document.getElementById("sword");
+        for(let i = 0; i < bugList.length; i++) {
+            if (doElsCollide(bugList[i], sword)) {
+                bugList[i].remove();
+                spawnNewBug();
+                let audio = new Audio('audio/swordhit.mp3');
+                audio.play();
+            }
+        }
+}
+
+function spawnNewBug(){
+    elem = document.createElement("img");
+    elem.style.position = "absolute";
+    elem.setAttribute("src", "images/bug.png");   
+    elem.classList.add("bug");
+    if(Math.random() < .5) { //top or bottom
+        elem.style.left = (window.innerWidth/10)*(Math.random() * 10) + "px";
+        if (Math.random() < .5) elem.style.top = "0px";
+        else elem.style.top = window.innerHeight + "px";
+    } else { //left or right
+        elem.style.top = (window.innerHeight/10)*(Math.random() * 10) + "px";
+        if (Math.random() < .5) elem.style.left = "0px";
+        else elem.style.left = window.innerWidth + "px";
+    }
+    let bugSize = playerSize + Math.trunc(Math.random() * (playerSize/2))
+    elem.setAttribute("height", bugSize);
+    elem.setAttribute("width", bugSize);
+    document.getElementById("body").prepend(elem.cloneNode(true))
 }
 
 function movePlayer(x, y){  
@@ -253,6 +292,44 @@ function moveBug(elem){
     else if (currentX < playerX) elem.style.left = currentX + bugMovement + "px";
     if (currentY > playerY) elem.style.top = currentY - bugMovement + "px";
     else if (currentY < playerY) elem.style.top = currentY + bugMovement + "px";
+}
+
+function checkPlayerCollision(){
+    let bugList = document.getElementsByClassName("bug");
+    let player = document.getElementById("player");
+        for(let i = 0; i < bugList.length; i++) {
+            if (doElsCollide(bugList[i], player)) {
+                processDamage();
+            }
+        }
+}
+
+function processDamage(){
+    let currentHurtTime = Date.now();
+    if (currentHurtTime - lastHurtTime < frameTime * 10) return;
+    hitsTaken++;
+    if (hitsTaken >= maxLives) {
+        let audio = new Audio('audio/gameover.mp3'); 
+        audio.play();
+        endGame();
+        return;
+    }
+    let player = document.getElementById("player");
+    let audio = new Audio('audio/hurt.mp3'); 
+    audio.volume = .3;
+    audio.play();
+    function hurtAnimation(){
+        if (Date.now() - currentHurtTime > frameTime * 10) return;
+        setTimeout(() => {
+            player.setAttribute("src", "images/computerhurt.png")
+        }, frameTime*2)
+        setTimeout(() => {
+            player.setAttribute("src", "images/computer.png")
+            hurtAnimation()
+        }, frameTime*4)
+    }
+    hurtAnimation()
+    lastHurtTime = currentHurtTime;
 }
 
 function doElsCollide (el1, el2) {
